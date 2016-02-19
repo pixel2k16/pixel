e8f86de7ffc2eb5bf879299d7a487eaf5aa79a8c
ramanareddysane <ramanareddysane20@gmail.com>
2016-02-19 16:00:29 +0530

    Changed contact details

--- Website/section5.php
+++ Website/section5.php
@@ -7,11 +7,11 @@
 			<div class="card">
 				<div class="card-img suresh"></div>
 				<div class="card-details">
-					<h2>P. Suresh Babu </h2>
+					<h2>Dr. A Suresh Babu </h2>
 					<div class="inner">
 						<span class="ocp">&#128100; Staff Coordinator</span><br>
-						<span class="phn">&#9742; 7382352503</span><br>
-						<span class="eml">&#9993; naveen_nnk9@yahoo.co.in</span><br>
+						<span class="phn">&#9742; 9440388093</span><br>
+						<span class="eml">&#9993; asureshjntu@gmail.com</span><br>
 					</div>
 				</div> <!-- Card details -->
 			</div><!-- Card -->
@@ -33,11 +33,11 @@
 			<div class="card">
 				<div class="card-img ramana"></div>
 				<div class="card-details">
-					<h2> S. Ramana Reddy </h2>
+					<h2> K Anil Kumar </h2>
 					<div class="inner">
 						<span class="ocp">&#128100; Student Coordinator</span><br>
-						<span class="phn">&#9742; 9553703647</span><br>
-						<span class="eml">&#9993; ramanareddysane</span><br>
+						<span class="phn">&#9742; 9393767927</span><br>
+						<span class="eml">&#9993; katam.anilkumar@gmail.com</span><br>
 					</div>
 				</div> <!-- Card details -->
 			</div><!-- Card -->
@@ -46,11 +46,11 @@
 			<div class="card">
 				<div class="card-img surya"></div>
 				<div class="card-details">
-					<h2> P. Surya Teja </h2>
+					<h2>D Manjunatha Reddy</h2>
 					<div class="inner">
 						<span class="ocp">&#128100; Student Coordinator</span><br>
-						<span class="phn">&#9742; 7382352503</span><br>
-						<span class="eml">&#9993; naveen_nnk9@yahoo.co.in</span><br>
+						<span class="phn">&#9742; 8466824059</span><br>
+						<span class="eml">&#9993; manju.dayyala@gmail.com</span><br>
 					</div>
 				</div> <!-- Card details -->
 			</div><!-- Card -->
